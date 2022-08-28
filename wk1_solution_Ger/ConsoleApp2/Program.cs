cc42a338f9457f2851ef0670508724f865b7d386
Ger <gbj.saris@avans.nl>
2022-08-28 18:58:56 +0200

    start

---
+++ wk1_solution_Ger/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World! program 2");
+
+        //String? input;
+        //input = Console.ReadLine();
+        //Console.WriteLine("you said: " + input);
+        //Console.Write("Press <Q> to quit... ");
+        //while (Console.ReadKey().Key != ConsoleKey.Q)
+        //{
+        //    Console.WriteLine("\r");
+        //    Console.Write("Press <Q> to quit... ");
+        //}
+
+        //Console.WriteLine("bye");
+        String? input = "niks";
+        while (input != "quit")
+        {
+            //try
+            //{
+                Console.WriteLine("type quit to end program");
+                input = Console.ReadLine();
+            //}
+
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("exception");
+            //}
+
+        }
+    }
+}
