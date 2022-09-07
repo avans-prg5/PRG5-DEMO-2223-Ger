71678a4738774bc6176d18216893acb31912a387
Ger <gbj.saris@avans.nl>
2022-09-07 21:18:57 +0200

    HelloWorld :banana:

---
+++ wk1_solution_GER_HelloWorld/MyDomain/Person.cs
@@ -0,0 +1,41 @@
+﻿namespace MyDomain
+{
+    public class Person
+    {
+        public string? Firstname { get; set; } = "";
+        public string? MiddleName { get; set; } = "";
+        public string? LastName { get; set; } = "";
+        public DateTime Birthdate { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return String.Concat(
+                    "|", Firstname, " ",
+                    String.Concat(MiddleName, " ").TrimStart(),
+                    LastName, "|");
+
+            }
+        }
+
+        public int Age
+        {
+            get {
+                int a = DateTime.Now.Year - Birthdate.Year;
+                if (DateTime.Now.Month == Birthdate.Month)
+                {
+                    if (DateTime.Now.Day < Birthdate.Day)
+                    {
+                        a--;
+                    }
+                }
+                else if (DateTime.Now.Day < Birthdate.Day)
+                {
+                    a--;
+                }
+                return a;
+            }
+        }
+    }
+}
---
+++ wk1_solution_GER_HelloWorld/wk1_solution_GER_HelloWorld/Program.cs
@@ -0,0 +1,51 @@
+﻿using MyDomain;
+
+namespace wk1_solution_GER_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //part 1
+            //Console.WriteLine("Enter your name: ");
+            //String? input;
+            //input = Console.ReadLine();
+            //Console.WriteLine("hello " + input);
+
+            //part 2
+            Person p = new Person();
+            Console.Write("Enter your  first name: ");
+            p.Firstname = Console.ReadLine();
+            Console.Write("Enter your middle name: ");
+            p.MiddleName = Console.ReadLine();
+            Console.Write("Enter your   last name: ");
+            p.LastName = Console.ReadLine();
+
+            bool correct = false;
+            DateTime bday = new DateTime(2000, 1, 1);
+
+            while (!correct)
+            {
+                Console.Write("Enter your date of birth (yyyy,m, d) : ");
+                string? instr = Console.ReadLine();
+                if (instr != null)
+                {
+                    try
+                    {
+                        bday = DateTime.Parse(instr);
+                        correct = true;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("wrong format, try again ");
+                        //try again
+                    }
+                }
+            }
+            p.Birthdate = bday;
+
+            Console.WriteLine("this is your age: " + p.Age);
+            //Console.WriteLine("hello " + p.FullName);
+        }
+    }
+}
