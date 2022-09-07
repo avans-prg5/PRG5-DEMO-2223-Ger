e52068cb91f444bc037bf6ed7435b8e643778e4d
Ger <gbj.saris@avans.nl>
2022-09-08 00:31:09 +0200

    :telephone: :book:

---
+++ wk1_solution_GER_Telefoonboekje/wk1_solution_GER_Telefoonboekje/Book.cs
@@ -0,0 +1,43 @@
+﻿using MyDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace wk1_solution_GER_Telefoonboekje
+{
+    internal class Book
+    {
+
+        //adding elements using collection-initializer syntax
+
+        List<Person> persons = new List<Person>()
+        {
+          new Person() { Firstname = "John",   LastName = "Drummajor",Birthdate = new DateTime(1943,03,29)},
+          new Person() { Firstname = "Blaise", LastName = "Pascal",   Birthdate = new DateTime(1623,06,19)},
+          new Person() { Firstname = "Peter",  LastName = "Gabriel",  Birthdate = new DateTime(1950,02,13)},
+          new Person() { Firstname = "Mister", LastName = "Ed",       Birthdate = new DateTime(1949,01,01)},
+          new Person() { Firstname = "Mister", LastName = "Zorro",    Birthdate = new DateTime(1957,10,01)},
+        };
+
+        public void PrintSortedByLastName()
+        {
+
+
+            IEnumerable<Person> mygroup1 = persons
+                .OrderByDescending(person => person.LastName);
+
+            IEnumerable<Person> mygroup2 = from p in persons orderby p.LastName descending select p;
+
+
+            foreach (var item in mygroup2)
+            {
+                Console.WriteLine(item.LastName);
+            }
+        }
+
+    }
+}
---
+++ wk1_solution_GER_Telefoonboekje/wk1_solution_GER_Telefoonboekje/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace wk1_solution_GER_Telefoonboekje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.PrintSortedByLastName();
+        }
+    }
+}
