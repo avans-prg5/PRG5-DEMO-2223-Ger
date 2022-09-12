46c4cdcdf6048b03b44d6c345aa3c78ebc7c030e
Ger <gbj.saris@avans.nl>
2022-09-12 23:38:06 +0200

    add  webproject to Telefoonboekje

---
+++ solution_GER_Telefoonboekje/webproject_GER_Telefoonboekje/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webproject_GER_Telefoonboekje.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
