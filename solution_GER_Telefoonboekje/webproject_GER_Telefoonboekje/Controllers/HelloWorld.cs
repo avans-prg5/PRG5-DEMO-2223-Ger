b338e1ee59c645b4bcacb02d915a6ff0fc12e03d
Ger <gbj.saris@avans.nl>
2022-09-13 20:45:19 +0200

    webproject added

---
+++ solution_GER_Telefoonboekje/webproject_GER_Telefoonboekje/Controllers/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webproject_GER_Telefoonboekje.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
