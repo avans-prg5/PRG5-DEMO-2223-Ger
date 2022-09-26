e18bd2c8a0a5fa0bf64d02c1334f0bdbeed062b7
Ger <gbj.saris@avans.nl>
2022-09-26 08:10:46 +0200

    added wk4_Solution_DBfirst

---
+++ wk4_Solution_DBfirst/wk4_ConsoleProject_DBfirst/Models/PhoneBookContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace wk4_ConsoleProject_DBfirst.Models
+{
+    public partial class PhoneBookContext : DbContext
+    {
+        public PhoneBookContext()
+        {
+        }
+
+        public PhoneBookContext(DbContextOptions<PhoneBookContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> Persons { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=NB211023;Initial Catalog=PhoneBook;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.ToTable("Person");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Birthdate).HasColumnType("date");
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ wk4_Solution_DBfirst/wk4_ConsoleProject_DBfirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using wk4_ConsoleProject_DBfirst.Models;
+
+namespace wk4_ConsoleProject_DBfirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- program start ---");
+            using (var context = new PhoneBookContext())
+            {
+                context.Persons.ToList().ForEach(p =>
+                {
+                    Console.WriteLine(p.FirstName + " " + p.LastName + " (age)");
+                });
+            }
+            Console.WriteLine("--- program end ---");
+        }
+    }
+}
