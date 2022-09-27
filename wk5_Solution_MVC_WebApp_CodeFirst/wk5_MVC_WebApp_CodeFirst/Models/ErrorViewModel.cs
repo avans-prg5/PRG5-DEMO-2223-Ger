fcd15ebe24be1be2b7230a1ccfcdc099e46ac734
Ger <gbj.saris@avans.nl>
2022-09-27 23:17:51 +0200

    added wk5_Solution_MVC_WebApp

---
+++ wk5_Solution_MVC_WebApp_CodeFirst/wk5_MVC_WebApp_CodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace wk5_MVC_WebApp_CodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
