5f62f115d978e7a6fa1bd49ce76b6552c54a5c9b
Ger <gbj.saris@avans.nl>
2022-10-07 11:51:11 +0200

    deleted unused demo

--- wk5_Solution_MVC_WebApp_CodeFirst/wk5_MVC_WebApp_CodeFirst/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace wk5_MVC_WebApp_CodeFirst.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
