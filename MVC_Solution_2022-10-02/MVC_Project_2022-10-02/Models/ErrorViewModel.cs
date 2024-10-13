8576c105077052efe9c4f1555f1993996f23e42f
Ger Saris <gbj.saris@avans.nl>
2024-10-13 21:46:23 +0200

    renamed, updated, cleaned, extended  Gers PhoneBookApp

--- MVC_Solution_2022-10-02/MVC_Project_2022-10-02/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_Project_2022_10_02.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
