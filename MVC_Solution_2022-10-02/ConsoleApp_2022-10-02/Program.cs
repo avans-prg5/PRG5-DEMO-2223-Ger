db12f142e92aa578aadd2ffa6987ec4e677825cb
Ger Saris <gbj.saris@avans.nl>
2024-10-13 19:53:04 +0200

    convert to .NET8

--- MVC_Solution_2022-10-02/ConsoleApp_2022-10-02/Program.cs
+++ MVC_Solution_2022-10-02/ConsoleApp_2022-10-02/Program.cs
@@ -16,38 +16,38 @@ namespace ConsoleApp_2022_10_02
 
             //}
 
-            
 
 
-            using (var context = new ContextFactory().CreateDbContext(new string[0]))
-            {
 
-                Console.WriteLine("= deleting all existing rows from DB PhoneBook =");
-                context.Persons.RemoveRange(context.Persons);
-                context.Hobbies.RemoveRange(context.Hobbies);
-                context.SaveChanges();
+            //    using (var context = new ContextFactory().CreateDbContext(new string[0]))
+            //    {
 
-                Console.WriteLine("= writing LocalBook content to PhoneBook =");
-                context.Persons.AddRange(new LocalBook().Persons);
-                context.SaveChanges();
+            //        Console.WriteLine("= deleting all existing rows from DB PhoneBook =");
+            //        context.Persons.RemoveRange(context.Persons);
+            //        context.Hobbies.RemoveRange(context.Hobbies);
+            //        context.SaveChanges();
 
-                Console.WriteLine("=== content ===");
-                context.Persons.ToList().ForEach(p =>
-                {
-                    Console.WriteLine(p.FullName);
+            //        Console.WriteLine("= writing LocalBook content to PhoneBook =");
+            //        context.Persons.AddRange(new LocalBook().Persons);
+            //        context.SaveChanges();
 
-                    if (p.Hobbies != null)
-                    {
-                        p.Hobbies.ToList().ForEach(h =>
-                        {
-                            Console.WriteLine("  :" + h.Titel);
-                        });
-                    }
-                    else { Console.WriteLine("  : <none>"); }
-                });
+            //        Console.WriteLine("=== content ===");
+            //        context.Persons.ToList().ForEach(p =>
+            //        {
+            //            Console.WriteLine(p.FullName);
+
+            //            if (p.Hobbies != null)
+            //            {
+            //                p.Hobbies.ToList().ForEach(h =>
+            //                {
+            //                    Console.WriteLine("  :" + h.Titel);
+            //                });
+            //            }
+            //            else { Console.WriteLine("  : <none>"); }
+            //        });
 
 
-            }
+            //    }
 
             Console.WriteLine("--- program end ----");
         }
