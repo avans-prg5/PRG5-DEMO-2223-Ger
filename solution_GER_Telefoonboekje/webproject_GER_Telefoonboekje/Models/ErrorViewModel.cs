365f8759001e86cdad6e1b228f751ce23a689711
Ger Saris <gbj.saris@avans.nl>
2024-10-13 21:52:09 +0200

    opruimen

--- solution_GER_Telefoonboekje/webproject_GER_Telefoonboekje/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace webproject_GER_Telefoonboekje.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
